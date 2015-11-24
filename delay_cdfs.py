746a60321908b37d2fe4828616bb12c092a62da9
Yannik Behr <yannik@yanmail.de>
2015-11-24 11:38:42 +0100

    initial commit

---
+++ delay_cdfs.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+"""
+Plot the cumulative distribution functions of delay measurements.
+Created on Aug 31, 2015
+
+@author: behry
+"""
+import matplotlib
+matplotlib.use('Agg')
+import json
+import numpy as np
+from pandas import Series, DataFrame
+import pandas as pd
+import matplotlib.pyplot as plt
+pd.options.display.mpl_style = 'default'
+
+fns = {'Turkey': ('./data/pick_delays_tr.txt', 'green', 'o'),
+       'Switzerland': ('./data/pick_delays_ch.txt', 'blue', 's'),
+       'Southern California': ('./data/pick_delays_ca.txt', 'red', '-'),
+       'Greece': ('./data/pick_delays_gr.txt', 'cyan', '^'),
+       'New Zealand': ('./data/pick_delays_nz.txt', 'black', '-.'),
+       'Iceland': ('./data/pick_delays_is.txt', 'brown', '--'),
+       'Romania': ('./data/pick_delays_ro.txt', 'orange', '*')}
+
+bw = True
+df = None
+all = {}
+fig = plt.figure(figsize=(10, 8))
+ax = fig.add_subplot(111)
+lines = []
+labels = []
+binmin = 0
+binmax = 30
+dbin = 1.0
+for _k in ['Southern California', 'Switzerland', 'New Zealand', 'Romania', 'Greece', 'Iceland', 'Turkey']:
+    _fn, _cl, _m = fns[_k]
+    fh = open(_fn)
+    pkdel = json.load(fh)
+    fh.close()
+    data = []
+    for _nst, _del in pkdel.iteritems():
+        data.extend(_del)
+    data = np.array(data)
+    if _k == 'New Zealand':
+        data -= 6.0
+    dmin = data.min()
+    dmax = data.max()
+    if dmin >= binmin and dmax <= binmax:
+        bins = np.arange(binmin, binmax + dbin, dbin)
+    elif dmin < binmin and dmax <= binmax:
+        bins = np.r_[dmin, np.arange(binmin, binmax + dbin, dbin)]
+    elif dmin >= binmin and dmax > binmax:
+        bins = np.r_[np.arange(binmin, binmax + dbin, dbin), dmax]
+    else:
+        bins = np.r_[dmin, np.arange(binmin, binmax + dbin, dbin), dmax]
+
+    if bw:
+        hist, edges = np.histogram(data, bins=bins, density=False)
+        x = edges[0:-1] + np.diff(edges)
+        y = np.cumsum(hist.astype(float)) / np.sum(hist)
+        if _m in ['-', '--', '-.', ':']:
+            ax.plot(x, y, color='k', linestyle=_m)
+            lines.append(plt.Line2D([], [], lw=2.0, linestyle=_m, color='k'))
+        else:
+            ax.plot(x, y, color='k', marker=_m)
+            lines.append(plt.Line2D([], [], lw=2.0, marker=_m, color='k'))
+    else:
+        ax.hist(data, cumulative=True, bins=bins, normed=True,
+                histtype='step', color=_cl)
+        lines.append(plt.Line2D([], [], lw=2.0, linestyle='-', color=_cl))
+    labels.append(_k)
+    all[_k] = Series(data)
+ax.set_xlim(0, 30)
+ax.set_xlabel('Pick delay [s]')
+ax.set_ylabel('Percentile')
+ax.set_yticks([0.0, 0.25, 0.5, 0.75, 1.0])
+ax.set_yticklabels([r'$0^{th}$', r'$25^{th}$', r'$50^{th}$',
+                    r'$75^{th}$', r'$100^{th}$'])
+ax.legend(lines, labels, loc='lower right', ncol=2)
+fig.savefig('./plots/pick_delay_cdf.pdf', bbox_inches='tight')
+df = DataFrame(all)
+plt.show()
